abc3b5858ce3653429558740018bfb3acc56df6a
Max Normand <maxnormand97@gmail.com>
2025-01-16 10:15:42 +1100

    work on get:id path

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,27 @@
-export default function MealDetailsPage() {
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css'
+
+export default function MealDetailsPage({params}) {
+
+  const meal = getMeal(params.mealSlug)
+
     return (
       <div>
-        <h1>Meal Details</h1>
+        <header className={classes.header}>
+          <div className={classes.image}></div>
+          <div className={classes.headerText}>
+            <h1>{meal.title}</h1>
+            <p className={classes.creator}>
+              by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            </p>
+            <p className={classes.summary}>{meal.summary}</p>
+          </div>
+        </header>
+        <main>
+          <p className={classes.instructions} dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}></p>
+        </main>
       </div>
     );
 }
---
+++ app/not-found.js
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <p>Sorry this does not exist!</p>
+    </main>
+}
